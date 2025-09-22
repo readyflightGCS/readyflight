22ec94c78588428d5110fb88e4531127b2926dad
COOK1EK1W1 <61564769+COOK1EK1W1@users.noreply.github.com>
2025-09-22 20:42:24 +0000

    add a bunch of files

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,38 @@
+const isElectron = import.meta.env.VITE_APP_PLATFOR === "electron";
+
+import Versions from './components/Versions'
+import electronLogo from './assets/electron.svg'
+
+
+function App(): React.JSX.Element {
+  const ipcHandle = (): void => { isElectron ? window.electron.ipcRenderer.send('ping') : null }
+
+  return (
+    <>
+      <img alt="logo" className="logo" src={electronLogo} />
+      <div className="creator">Powered by electron-vite</div>
+      <div className="text">
+        Build kjhasdkjhskdjhas an Electron app with <span className="react">React</span>
+        &nbsp;and <span className="ts">TypeScript</span>
+      </div>
+      <p className="tip">
+        Please try pressing <code>F12</code> to open the devTool
+      </p>
+      <div className="actions">
+        <div className="action">
+          <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
+            Documentation
+          </a>
+        </div>
+        <div className="action">
+          <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
+            Send IPC
+          </a>
+        </div>
+      </div>
+      {isElectron ? <Versions /> : null}
+    </>
+  )
+}
+
+export default App
