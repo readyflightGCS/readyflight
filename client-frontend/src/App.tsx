f93b6357ac9800310bbe314569e818ba417f0bd0
COOK1EK1W1 <61564769+COOK1EK1W1@users.noreply.github.com>
2025-11-16 15:13:26 +0000

    fix styling for map behind ui

--- client-frontend/src/App.tsx
+++ client-frontend/src/App.tsx
@@ -1,8 +1,6 @@
 const isElectron = import.meta.env.VITE_TARGET === 'electron'
 
-import { Button } from './components/ui/button'
 import Map from "./components/ui/map"
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 import SideBar from '@/components/navigation/sidebar'
 import SidePanel from './components/navigation/sidePanel/sidePanel'
@@ -20,25 +18,17 @@ export default function App(): React.JSX.Element {
 
     <div className="flex flex-cols-2 h-full w-full bg-gray-200 text-foreground">
       <SideBar />
-      <div className="flex-grow relative">
 
-        {/* the overlay div */}
-        <div className="flex h-full absolute inset-0 ">
-          <div>
-            <SidePanel />
-          </div>
-          <div className="flex-grow flex flex-col">
-            <div className="flex-grow">
-            </div>
-            <BottomPanel />
-          </div>
+      <div className="flex-grow relative">
+        <div className="absolute top-0 left-0 h-full z-20">
+          <SidePanel />
         </div>
 
-        { /*map component goes here (temporary grid for now :))*/}
-        {/* <div className="w-full h-full bg-[linear-gradient(90deg,#c2c2c2_1px,transparent_1px),linear-gradient(#c2c2c2_1px,transparent_1px)] bg-[size:20px_20px]"></div>*/}
-        { /* or a gradient to test overlaying stuff */}
-        {/*<div className="w-full h-full bg-[radial-gradient(circle_at_top_left,#ff8a80,#80d8ff)]"></div>*/}
-        <Map/>
+        <div className="absolute bottom-0 left-64 right-0 z-20 pointer-events-auto">
+          <BottomPanel />
+        </div>
+
+        <Map />
       </div>
     </div>
   )
