6e3143dd30c9704aa0ddc656e10a651413bc0ec5
COOK1EK1W1 <61564769+COOK1EK1W1@users.noreply.github.com>
2026-01-05 08:00:03 +0000

    add tabs to missino bottom panel

--- client-frontend/src/components/navigation/bottomPanel/mission.tsx
+++ client-frontend/src/components/navigation/bottomPanel/mission.tsx
@@ -1,11 +1,19 @@
 import { Button } from "@/components/ui/button"
 import { tools, useMission } from "@/stores/mission"
 import ParamEditor from "./mission/paramEdit"
+import { ReactNode, useState } from "react"
+import { Separator } from "@/components/ui/separator"
 
+const tabs = [
+  { name: "Parameter", component: <ParamEditor /> },
+  { name: "Terrain", component: <div /> },
+  { name: "Waypoint Check", component: <div /> }
+] as const satisfies { name: string, component: ReactNode }[]
 
 export default function mission() {
   const setTool = useMission((state => state.setTool))
   const currentTool = useMission((state) => state.tool)
+  const [currentTab, setCurrentTab] = useState<typeof tabs[number]["name"]>("Parameter")
   return (
     <div>
       <div className="flex justify-between">
@@ -15,7 +23,16 @@ export default function mission() {
           </Button>
         ))}
       </div>
-      <ParamEditor />
+      <div className="flex flex-row">
+        <div className="flex flex-col">
+          {tabs.map((tab, i) => (
+            <Button key={i} variant={currentTab === tab.name ? "active" : "default"} onClick={() => setCurrentTab(tab.name)}>{tab.name}</Button>
+          ))}
+
+        </div>
+        <Separator orientation="vertical" />
+        {tabs.find(x => x.name === currentTab).component}
+      </div>
     </div>
 
   )
