ecd7b161438f34dd3dc30efd844f6868f5a467a9
COOK1EK1W1 <61564769+COOK1EK1W1@users.noreply.github.com>
2026-05-18 21:53:59 +0100

    add scroll area and fix styles

--- client-frontend/src/components/navigation/sidePanel/mission/commandList.tsx
+++ client-frontend/src/components/navigation/sidePanel/mission/commandList.tsx
@@ -1,5 +1,6 @@
 import { useMission } from '@libs/stores/mission'
 import { Button } from '@/components/ui/button'
+import { ScrollArea } from '@/components/ui/scroll-area'
 import { DialectCommandDescription, MissionCommand } from '@libs/commands/command'
 import CommandItem from './commandItem'
 import { getCommandLocation } from '@libs/commands/helpers'
@@ -110,53 +111,55 @@ export default function CommandList() {
   let cmdId = 0
 
   return (
-    <div className="min-h-0 overflow-y-auto ">
-      {!hasTakeoff && selectedSubMission == 'Main' ? (
-        <div className="py-2">
-          <Button
-            name="Add Takeoff"
-            onClick={createTakeoff}
-            className="text-center w-full my-2 mx-0 h-12"
-          >
-            Add Takeoff
-          </Button>
-        </div>
-      ) : null}
+    <div className="min-h-0">
+      <ScrollArea className="h-full">
+        {!hasTakeoff && selectedSubMission == 'Main' ? (
+          <div className="py-2">
+            <Button
+              name="Add Takeoff"
+              onClick={createTakeoff}
+              className="text-center w-full my-2 mx-0 h-12"
+            >
+              Add Takeoff
+            </Button>
+          </div>
+        ) : null}
 
-      {missionWithDubinsGroups.map((node, i) => {
-        if (node.type === 'cmd') {
-          return <CommandItem id={cmdId++} key={i} command={node.cmd} />
-        } else {
-          return (
-            <div key={i} className="pl-2">
-              <div className="relative pl-2">
-                <div className="absolute left-0 top-1 bottom-1 w-[2px] bg-red-500 rounded-full" />
+        {missionWithDubinsGroups.map((node, i) => {
+          if (node.type === 'cmd') {
+            return <CommandItem id={cmdId++} key={i} command={node.cmd} />
+          } else {
+            return (
+              <div key={i} className="pl-2">
+                <div className="relative pl-2">
+                  <div className="absolute left-0 top-1 bottom-1 w-[2px] bg-red-500 rounded-full" />
 
-                <div>
-                  {node.cmds.map((cmd, j) => (
-                    <CommandItem id={cmdId++} key={j} command={cmd} />
-                  ))}
+                  <div>
+                    {node.cmds.map((cmd, j) => (
+                      <CommandItem id={cmdId++} key={j} command={cmd} />
+                    ))}
+                  </div>
                 </div>
               </div>
-            </div>
-          )
-        }
-      })}
-      {selectedCommandIDs.length > 1 ? (
-        <div className="w-full flex justify-center">
-          <Button variant="active" onMouseDown={handleGroup} className="text-center p-1 m-1 w-44">
-            Group {selectedCommandIDs.length} waypoints
-          </Button>
-        </div>
-      ) : null}
+            )
+          }
+        })}
+        {selectedCommandIDs.length > 1 ? (
+          <div className="w-full flex justify-center">
+            <Button variant="active" onMouseDown={handleGroup} className="text-center p-1 m-1 w-44">
+              Group {selectedCommandIDs.length} waypoints
+            </Button>
+          </div>
+        ) : null}
 
-      {!hasLanding && selectedSubMission == 'Main' ? (
-        <div className="py-2">
-          <Button onClick={createLanding} className="w-full my-2 mx-0 h-12">
-            Add Landing
-          </Button>
-        </div>
-      ) : null}
+        {!hasLanding && selectedSubMission == 'Main' ? (
+          <div className="py-2">
+            <Button onClick={createLanding} className="w-full my-2 mx-0 h-12">
+              Add Landing
+            </Button>
+          </div>
+        ) : null}
+      </ScrollArea>
     </div>
   )
 }
