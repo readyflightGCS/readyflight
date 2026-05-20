fb14315442e18c56366bca93557562747f4d8166
COOK1EK1W1 <61564769+COOK1EK1W1@users.noreply.github.com>
2026-05-20 14:39:41 +0100

    removed some files

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -6,27 +6,27 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-      starlight({
-          title: 'ReadyFlight',
-          social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/readyflightGCS/readyflight' }],
-          sidebar: [
-              {
-                  label: 'ReadyFlight',
-                  items: [
-                      // Each item here is one entry in the navigation menu.
-                      { label: 'Getting Started', slug: 'getting-started' },
-                  ],
-              }
-          ],
-		  customCss: [
-        	'./src/styles/global.css',
-            './src/styles/landing.css',
-      	  ],
-      }),
-	],
+    integrations: [
+        starlight({
+            title: 'ReadyFlight',
+            social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/readyflightGCS/readyflight' }],
+            sidebar: [
+                {
+                    label: 'ReadyFlight',
+                    items: [
+                        // Each item here is one entry in the navigation menu.
+                        { label: 'Getting Started', slug: 'getting-started' },
+                    ],
+                }
+            ],
+            customCss: [
+                './src/styles/global.css',
+                './src/styles/landing.css',
+            ],
+        }),
+    ],
 
-  vite: {
-    plugins: [tailwindcss()],
-  },
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
