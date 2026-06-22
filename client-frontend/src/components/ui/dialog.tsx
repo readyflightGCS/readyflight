e7ca7ed2753a5cbf9d685102e395f12798e34c64
COOK1EK1W1 <61564769+COOK1EK1W1@users.noreply.github.com>
2026-06-22 10:14:42 +0100

    fix dialog title

--- client-frontend/src/components/ui/dialog.tsx
+++ client-frontend/src/components/ui/dialog.tsx
@@ -93,7 +93,7 @@ function DialogTitle({ className, ...props }: React.ComponentProps<typeof Dialog
   return (
     <DialogPrimitive.Title
       data-slot="dialog-title"
-      className={cn('text-lg leading-none font-semibold', className)}
+      className={cn('text-foreground text-lg leading-none font-semibold', className)}
       {...props}
     />
   )
